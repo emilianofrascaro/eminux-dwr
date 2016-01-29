f66553479285b56880ecf90a204ae90082d68eb3
frem0 <frem0@WS7061.itdomain.net>
2016-01-29 17:19:22 +0100

    First import of DWR 3.x plugin for Grails 2.x

---
+++ src/java/org/eminux/dwr/test/Salutatore.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.eminux.dwr.test;
+
+/**
+ *
+ * @author frem0
+ */
+public class Salutatore {
+    
+    public Salutatore(){
+        
+    }
+    
+    public String saluta(String nome){
+        return "Ciao "+nome;
+    }
+    
+}
